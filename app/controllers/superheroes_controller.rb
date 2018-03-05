772d656b4d905917f984d422fe61975c27077aec
github username <github email address>
2018-03-05 10:40:42 -0500

    finsihed

--- app/controllers/superheroes_controller.rb
+++ app/controllers/superheroes_controller.rb
@@ -2,4 +2,37 @@ class SuperheroesController < ApplicationController
   def index
     @superheroes = Superhero.all
   end
+
+  def show
+    @superhero = Superhero.find(params[:id])
+  end
+
+  def new
+    @superhero = Superhero.new
+  end
+
+  def create
+    @superhero = Superhero.new(superhero_params)
+    @superhero.superpower = Superpower.find(params[:superhero][:superpower])
+
+    if @superhero.valid?
+      @superhero.save
+      redirect_to @superhero
+    else
+      render :new
+    end
+  end
+
+  def search
+    @superpower = params[:superpower]
+    @superheroes = Superhero.all.select do |superhero|
+                    superhero.superpower.name == params[:superpower]
+                  end
+  end
+
+  private
+
+  def superhero_params
+    params.require(:superhero).permit(:name, :super_name)
+  end
 end
