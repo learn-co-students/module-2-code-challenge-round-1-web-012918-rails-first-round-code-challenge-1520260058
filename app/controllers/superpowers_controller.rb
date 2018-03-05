898aabde39a604bc5d2d405ea5869df3a8772db5
aalexander3 <aralx73@gmail.com>
2018-03-05 10:43:29 -0500

    done.

--- app/controllers/superpowers_controller.rb
+++ app/controllers/superpowers_controller.rb
@@ -2,4 +2,13 @@ class SuperpowersController < ApplicationController
   def index
     @superpowers = Superpower.all
   end
+
+  def show
+    @superpower = Superpower.find(params[:id])
+  end
+
+  def self.search(q)
+    Superpower.where("name LIKE ?", "%#{q}%")
+  end
+
 end
--- app/models/superpower.rb
+++ app/models/superpower.rb
@@ -1,2 +1,8 @@
 class Superpower < ApplicationRecord
+  has_many :superheroes
+
+  def self.search(q)
+    Superpower.where("name LIKE ?", "%#{q}%")
+  end
+
 end
