2d0d0095be5a31721b14d8911d1117c7abd19307
alin684 <al684@cornell.edu>
2018-03-05 10:44:05 -0500

    done

--- app/controllers/superheroes_controller.rb
+++ app/controllers/superheroes_controller.rb
@@ -1,5 +1,29 @@
 class SuperheroesController < ApplicationController
   def index
     @superheroes = Superhero.all
+    @superpowers = Superpower.all
+  end
+
+  def new
+    @superhero = Superhero.new
+  end
+
+  def create
+    @superhero = Superhero.new(superhero_params)
+    if @superhero.save
+      redirect_to superhero_path(@superhero)
+    else
+      render :new
+    end
+  end
+
+  def show
+    @superhero = Superhero.find(params[:id])
+  end
+
+  private
+
+  def superhero_params
+    params.require(:superhero).permit(:name, :super_name, :superpower_id, :superpower)
   end
 end
