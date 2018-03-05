0b30d30227270198ce259f701010b174ed5a7962
github username <github email address>
2018-03-05 10:36:00 -0500

    Done!

--- app/controllers/superheroes_controller.rb
+++ app/controllers/superheroes_controller.rb
@@ -1,5 +1,54 @@
 class SuperheroesController < ApplicationController
+  before_action :get_hero, only: [:show, :edit, :update, :destroy]
+
+  def show
+  end
+
   def index
     @superheroes = Superhero.all
   end
+
+  def new
+    @superhero = Superhero.new
+  end
+
+  def create
+    @superhero = Superhero.new(hero_params)
+    if @superhero.valid?
+       @superhero.save
+        redirect_to @superhero
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @superhero.update(hero_params)
+      redirect_to @superhero
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @superhero.destroy
+    redirect_to superheroes_path
+  end
+
+
+  private
+
+  def get_hero
+    @superhero = Superhero.find(params[:id])
+  end
+
+  def hero_params
+    params.require(:superhero).require[:name, :super_name, :superpower_id]
+
+
+  end
+
 end
--- app/controllers/superpowers_controller.rb
+++ app/controllers/superpowers_controller.rb
@@ -1,5 +1,52 @@
 class SuperpowersController < ApplicationController
+  before_action :get_superpower, only: [:show, :edit, :update, :destroy]
+
+  def show
+  end
+
   def index
+    byebug
     @superpowers = Superpower.all
   end
+
+  def new
+    @superpower = Superpower.new
+  end
+
+  def create
+    @superpower = Superpower.new(super_hero_params)
+    if @superpower.valid?
+       @superpower.save
+        redirect_to @superpower
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @superpower.update(super_hero_params)
+      redirect_to @superpower
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @superpower.destroy
+    # redirect_to superheroes_path
+  end
+
+
+  private
+  def get_superpower
+    @superpower = Superpower.find(params[:id])
+  end
+
+  def super_hero_params
+    params.require(:superpower).require[:name, :description]
+  end
+
 end
