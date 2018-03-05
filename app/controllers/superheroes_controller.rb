1993781e11b0a7bfb30f81bdcb7f8bb91762582f
johnvoyage <jmyork762@gmail.com>
2018-03-05 10:26:07 -0500

    John's commit

--- app/controllers/superheroes_controller.rb
+++ app/controllers/superheroes_controller.rb
@@ -1,5 +1,42 @@
 class SuperheroesController < ApplicationController
+
   def index
     @superheroes = Superhero.all
   end
+
+  def show
+    @superhero = Superhero.find(params[:id])
+  end
+
+  def new
+    @superhero = Superhero.new
+  end
+
+  def create
+    @superhero = Superhero.create(
+      name: params[:superhero][:name],
+      super_name: params[:superhero][:super_name],
+      superpower_id: params[:superpower_id]
+    )
+    if @superhero.save
+      redirect_to @superhero
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @superhero = Superhero.find(params[:id])
+    @superhero.destroy
+    redirect_to superheroes_path
+  end
+
+  def filtered
+    @superpower = params[:search]
+    @superheroes_w_superpower = Superhero.all.select do |sh|
+      sh.superpower_id == Superpower.find_by(name: @superpower).id
+    end
+    render :filtered
+  end
+
 end
--- app/models/superhero.rb
+++ app/models/superhero.rb
@@ -1,2 +1,15 @@
 class Superhero < ApplicationRecord
+
+  has_one :superpower
+
+  validates :super_name, uniqueness: true
+
+  def superpower_name
+    Superpower.find(superpower_id).name
+  end
+
+  def superpower_object
+    Superpower.find(superpower_id)
+  end
+
 end
