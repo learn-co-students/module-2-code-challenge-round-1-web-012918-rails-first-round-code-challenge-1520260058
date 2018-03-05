bbf36e28da090ecf743860a58dbafa983252e9f7
scott <you@example.comitsmescottb123@gmail.com>
2018-03-05 10:39:26 -0500

    done

--- app/controllers/superheroes_controller.rb
+++ app/controllers/superheroes_controller.rb
@@ -2,4 +2,32 @@ class SuperheroesController < ApplicationController
   def index
     @superheroes = Superhero.all
   end
+
+  def show
+    get_superhero
+  end
+
+  def new
+    @superhero = Superhero.new
+  end
+
+  def create
+    @superhero = Superhero.create(superhero_params)
+    if @superhero.save
+      redirect_to @superhero
+    else
+      render :new
+    end
+  end
+  
+  private
+
+  def superhero_params
+    params.require(:superhero).permit(:name, :super_name, :superpower_id)
+  end
+
+  def get_superhero
+    @superhero = Superhero.find(params[:id])
+  end
+
 end
--- app/models/superhero.rb
+++ app/models/superhero.rb
@@ -1,2 +1,14 @@
 class Superhero < ApplicationRecord
+  belongs_to :superpower
+
+  validates :super_name, uniqueness: true
+
+  def self.search(query)
+    if query.present?
+      where("name LIKE ?", "%#{query}")
+    else
+      find(:all)
+    end
+  end
+
 end
