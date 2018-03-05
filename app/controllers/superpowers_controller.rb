5f5904ce432bb71ae5be6f2466bef20f33de2dbc
Zahran Kabir <zahran@Zahrans-MacBook-Pro.local>
2018-03-05 10:44:41 -0500

    done

--- app/controllers/superpowers_controller.rb
+++ app/controllers/superpowers_controller.rb
@@ -1,5 +1,39 @@
 class SuperpowersController < ApplicationController
-  def index
-    @superpowers = Superpower.all
-  end
+
+  before_action :get_superpower, only: [:show]
+
+    def index
+      @superpowers = Superpower.all
+    end
+
+    def new
+      @superpower = Superpower.new
+    end
+
+    def create
+    @superpower = Superpower.new(superpower_params)
+    if @superpower.valid?
+      @superpower.save
+      redirect_to superpower_path(@superpower)
+    else
+      render :new
+    end
+
+    end
+
+    def show
+
+    end
+
+
+    private
+
+    def superpower_params
+      params.require(:superpower).permit(:name, :description)
+    end
+
+    def get_superpower
+      @superpower = Superpower.find(params[:id])
+    end
+
 end
--- app/models/superhero.rb
+++ app/models/superhero.rb
@@ -1,2 +1,6 @@
 class Superhero < ApplicationRecord
+  validates :name, presence: true
+  validates :super_name, uniqueness: true
+  
+  belongs_to :superpower
 end
