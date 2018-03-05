ab6c6262d0b77d9989aa75c7b95d430eaf04a012
rfarney3 <farneyr@yahoo.com>
2018-03-05 10:32:20 -0500

    Full Project

--- app/controllers/superheroes_controller.rb
+++ app/controllers/superheroes_controller.rb
@@ -1,5 +1,50 @@
 class SuperheroesController < ApplicationController
+  before_action :get_superhero, only: [:show, :edit, :update, :destroy]
+
   def index
-    @superheroes = Superhero.all
+    @superheroes = Superhero.search(params[:term])
+  end
+
+  def show
+  end
+
+  def new
+    @superhero = Superhero.new
+  end
+
+  def create
+    @superhero = Superhero.create(superhero_params)
+
+    if @superhero.save
+      redirect_to superhero_path(@superhero)
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @superhero.update(superhero_params)
+      redirect_to superhero_path(@superhero)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @superhero.destroy
+    redirect_to superheroes_path
+  end
+
+  private
+
+  def get_superhero
+    @superhero = Superhero.find(params[:id])
+  end
+
+  def superhero_params
+    params.require(:superhero).permit(:name, :super_name, :superpower_id, :term)
   end
 end
--- app/models/superhero.rb
+++ app/models/superhero.rb
@@ -1,2 +1,13 @@
 class Superhero < ApplicationRecord
+  belongs_to :superpower
+  validates :super_name, uniqueness: true
+  validates :super_name, presence: true
+
+  def self.search(term)
+    if term
+      Superhero.joins(:superpower).where('superpowers.name LIKE ?', "%#{term}%")
+    else
+      Superhero.all
+    end
+  end
 end
