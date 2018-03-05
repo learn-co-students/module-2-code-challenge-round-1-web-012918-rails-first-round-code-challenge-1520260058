20fa4de4c398817bbb618960c2510aeba9b609d7
jaf7 <janthonyfields@gmail.com>
2018-03-05 10:40:22 -0500

    done

--- app/controllers/superheroes_controller.rb
+++ app/controllers/superheroes_controller.rb
@@ -1,5 +1,45 @@
 class SuperheroesController < ApplicationController
+
+  before_action :set_superhero, only: [:show, :search]
   def index
     @superheroes = Superhero.all
   end
+
+  def new
+    @superhero = Superhero.new
+  end
+
+  def create
+    @superhero = Superhero.new(superhero_params)
+    # byebug
+    if @superhero.save
+      redirect_to superhero_path(@superhero)
+    else
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  private
+
+  def superhero_params
+    params.require(:superhero).permit(:name, :super_name, :superpower_id)
+  end
+
+  def set_superhero
+    @superhero = Superhero.find(params[:id])
+  end
+
 end
+
+
+# // ☆  rake routes
+#        Prefix Verb URI Pattern                Controller#Action
+#   superpowers GET  /superpowers(.:format)     superpowers#index
+#    superpower GET  /superpowers/:id(.:format) superpowers#show
+#   superheroes GET  /superheroes(.:format)     superheroes#index
+#               POST /superheroes(.:format)     superheroes#create
+# new_superhero GET  /superheroes/new(.:format) superheroes#new
+#     superhero GET  /superheroes/:id(.:format) superheroes#show
--- app/controllers/superpowers_controller.rb
+++ app/controllers/superpowers_controller.rb
@@ -1,5 +1,31 @@
 class SuperpowersController < ApplicationController
+
+  before_action :set_superpower, only: [:show]
   def index
     @superpowers = Superpower.all
   end
+
+  def show
+  end
+
+  def search
+    superpower_name = params[:q]
+    @superheroes = Superhero.all.select {|sh| Superpower.find(sh.superpower_id).name == superpower_name}
+    if @superheroes.any?
+      redirect_to superheroes_path
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def set_superpower
+    @superpower = Superpower.find(params[:id])
+  end
+
+  def superpower_params
+    params.require(:superpower).permit(:name, :description)
+  end
+
 end
